b82bd1c12e85e8a87678d4749433170adffe976d
swastikrout7 <swastikrout1@gmail.com>
2022-03-14 17:17:07 +0530

    Second Commit Added hello in calculator

--- FirstMavenExample/src/main/java/com/lti/demo/TestCalc.java
+++ FirstMavenExample/src/main/java/com/lti/demo/TestCalc.java
@@ -3,6 +3,7 @@ package com.lti.demo;
 public class TestCalc {
 
 	public static void main(String[] args) {
+		System.out.println("Hello This is your Calculator");
 		Calculator c = new Calculator();
 		int r = c.add(200,300);
 		System.out.println(r);
